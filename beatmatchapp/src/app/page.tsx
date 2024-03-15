dd1300e1e25ed52ae7913162c57a45c86b205f5e
Jason G <124753175+ThisIsGoodMan@users.noreply.github.com>
2024-03-14 17:07:27 -0700

    Update

--- beatmatchapp/src/app/page.tsx
+++ beatmatchapp/src/app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <div>
+    <>
+      <div id="welcome-banner">
       <h1>Welcome to BeatMatchApp!</h1>
       <h3>Leave the crate digging to us!</h3>
-    </div>
+      </div>
+      <form action="sumbit">
+        <h2></h2>
+        <textarea name="spotifylink" id="spotifylink" defaultValue="Enter Spotify URL" cols={30} rows={10} />
+        <br />
+        <button>Start Digging</button>
+      </form>
+    </>
   );
 }
